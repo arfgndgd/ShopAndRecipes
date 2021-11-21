eb3d7364a3ea5a03c6a5d6c84d72e7fb2926f87a
arfgndgd <gundogduarif4@gmail.com>
2021-11-21 18:06:05 +0300

    init 6.04

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Ingredient } from "../shared/Ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [ //recipe.model.ts'e göre parametre vermemiz gerek; name, description, image
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A super-tasty Schnitzel - just awesome',
+      'https://i.lezzet.com.tr/images-xxlarge-recipe/et_schnitzel-acb28d38-df55-49bc-a0a3-79de31b288ca.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger', 
+      'What else you need to say',
+      'https://www.bysekerci.com/wp-content/uploads/2020/08/hamburger-menu-kastamonu-tasarim-com.jpg',
+      [
+        new Ingredient('Buns', 4),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index]; 
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
