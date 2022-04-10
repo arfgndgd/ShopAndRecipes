101e576908a2614504a5a3b8353006baf29cf710
arfgndgd <arif.gundogdu@isimplatform.io>
2022-04-10 16:53:51 +0300

    Auth vs

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
-import { Subject, throwError } from "rxjs";
+import { BehaviorSubject, Subject, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 export interface AuthResponseData {
     kind: string;
@@ -16,9 +17,9 @@ export interface AuthResponseData {
 }
 @Injectable({providedIn: 'root'})
 export class AuthService {
-    user = new Subject<User>();
-    
-    constructor(private http: HttpClient) {}
+    user = new BehaviorSubject<User>(null);
+
+    constructor(private http: HttpClient, private router: Router) {}
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
@@ -45,6 +46,11 @@ export class AuthService {
         ).pipe(catchError(this.handleError),tap(resData => {
             this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
         }));
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
